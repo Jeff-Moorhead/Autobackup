fc2413c0446e8ae5c4c3a81d920581c397cfee56
Jeff-Moorhead <Jeff.Moorhead1@gmail.com>
2018-06-09 08:36:11 -0400

    Removed hradcoding of drives. Backup now uses command line args

--- autobackup.py
+++ autobackup.py
@@ -6,35 +6,36 @@ running in the background.
 """
 
 import os
-from sys import exit
+import sys
 from time import sleep
 from backup import backup
 
 documents = "C:\\Users\\Jmoor\\OneDrive\\Documents"
-desktop = "C:\\Users\\Jmoor\\Desktop"
-
+desktop = "C:\\Users\\Jmoor\\OneDrive\\Desktop"
+drive1 = sys.argv[1]
+drive2 = sys.argv[2]
 # Check if computer should shut down after backups.
 shutdown = input('Shut down after backups (y/n)? ')
 
 # If neither backup drive exists, shutdown the computer.
-if not (os.path.exists("F:\\") or os.path.exists("E:\\")):
+if not (os.path.exists(drive1 + '\\') or os.path.exists(drive2 + '\\')):
 	print("---------- Backup failed: Drives not available! ----------")
 	sleep(3)
-	exit(1)
+	sys.exit(1)
 else:
 	# Perform backups.
-	if os.path.exists("E:\\"):
-		backup(desktop, "E:\\")
-		backup(documents, "E:\\")
+	if os.path.exists(f'{drive1}\\'):
+		backup(desktop, f'{drive1}\\')
+		backup(documents, f'{drive1}\\')
 		#backup("C:\\Users\\Jeff Moorhead\\Pictures", "E:\\")
 		
-	if os.path.exists("F:\\"):
-		backup(desktop, "F:\\")
-		backup(documents, "F:\\")
+	if os.path.exists(f'{drive2}\\'):
+		backup(desktop, f'{drive2}\\')
+		backup(documents, f'{drive2}\\')
 		#backup("C:\\Users\\Jeff Moorhead\\Pictures", "F:\\")
 
 if shutdown == 'y':
-        os.system('shutdown /s /t 10')
+        os.system('shutdown /s /t 3')
 else:
-        sleep(5)
-        exit(0)
+        sleep(3)
+        sys.exit(0)
--- backup.py
+++ backup.py
@@ -18,15 +18,20 @@ def backup(src, dest):
 
     if os.path.exists(newFile):
         os.unlink(newFile)
+        newFile = f'{base}_{now.month}-{now.day}-{now.year}_OVERWRITE.zip'
 
     # Write the zipfile and walk the source directory tree.
     with zipfile.ZipFile(newFile, 'w') as zip:
-        for folder, subfolders, files in os.walk(src):
+        for folder, _ , files in os.walk(src):
             print(f'Working in folder {os.path.basename(folder)}')
+
             for file in files:
                 zip.write(os.path.join(folder, file),
+                          arcname=os.path.join(
+                              folder[len(src):], file),
                           compress_type=zipfile.ZIP_DEFLATED)
-        print(f'\n---------- Backup of {base} to {dest} successful! ----------\n')
+        print(
+            f'\n---------- Backup of {base} to {dest} successful! ----------\n')
 
 
 if __name__ == '__main__':
